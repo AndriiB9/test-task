f6d3b17fcecd6302d8323b0ba652117bb191be2b
Andrii Bakai <andriibakai@gmail.com>
2020-05-09 09:22:46 +0300

    Adding test task

---
+++ test_task.js
@@ -0,0 +1,50 @@
+function attempt(available, allowed, preferred) {
+    let common = available.filter(value => allowed.includes(value) || allowed.includes('any'));
+    let result = [];
+    if (preferred.includes('any')) {
+        return common;
+    }
+    for (preferredItem of preferred) {
+        if (common.includes(preferredItem)) {
+            addElement(preferredItem);
+        } else {
+            if (common[length - 1] < preferredItem) {
+                addElement(common[length - 1]);
+            } else if (common[0] > preferredItem) {
+                addElement(common[0]);
+            } else {
+                let mid = Math.floor(common.length / 2);
+                while(mid > 0) {
+                    if (common[mid] > preferredItem) {
+                        if (common[mid - 1] < preferredItem) {
+                            addElement(common[mid]);
+                            break;
+                        } else {
+                            mid = Math.floor(mid / 2);
+                            continue;
+                        }
+                    } else {
+                        if (common[mid + 1] > preferredItem) {
+                            addElement(common[mid + 1]);
+                            break;
+                        } else {
+                            mid += Math.floor((length - mid - 1) / 2);
+                            continue;
+                        }
+                    }
+                }
+            }          
+        } 
+    }
+    return result;
+
+    function addElement(element) {
+        if (!result.includes(element)) {
+            result.push(element)
+        }
+    }
+}
+
+
+
+console.log(attempt([240, 360, 720], [360, 720, 1080], [220, 230, 720]))
